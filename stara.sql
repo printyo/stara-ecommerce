d543b7ea7d11406ff32db3c977a77aacd697a502
Print Yongsuphamongkol <print.krittanut@gmail.com>
2024-10-27 23:32:05 +0700

    database-v1
    stara database with tables and keys only, no inputs.

---
+++ stara.sql
@@ -0,0 +1,346 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 27, 2024 at 05:27 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stara`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `userID` int(11) NOT NULL,
+  `productID` int(11) NOT NULL,
+  `quantity` int(3) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryID` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devChat`
+--
+
+CREATE TABLE `devChat` (
+  `devChatID` int(11) NOT NULL,
+  `chat` varchar(255) NOT NULL COMMENT 'Discuss Problems with Developer',
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderDetails`
+--
+
+CREATE TABLE `orderDetails` (
+  `orderID` int(11) NOT NULL,
+  `Total` decimal(11,2) NOT NULL,
+  `AddressID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderItems`
+--
+
+CREATE TABLE `orderItems` (
+  `orderID` int(11) NOT NULL,
+  `productID` int(11) NOT NULL,
+  `quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderStatusHistory`
+--
+
+CREATE TABLE `orderStatusHistory` (
+  `orderStatusID` int(11) NOT NULL,
+  `dateTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '1=pending,2=payment confirmed, 3=processing, 4=shipped, 5=delivered, 6=payment failed',
+  `remark` varchar(255) DEFAULT NULL,
+  `orderID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `paymentID` int(11) NOT NULL,
+  `fileURL` varchar(255) NOT NULL DEFAULT 'n-a.jpg' COMMENT 'store payment-slips path',
+  `dateTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `orderID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `productID` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` decimal(11,2) NOT NULL,
+  `imageURL` varchar(255) NOT NULL DEFAULT 'n-a.jpg' COMMENT 'image path eg. t-shirt.jpg',
+  `categoryID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(30) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Role` tinyint(4) NOT NULL COMMENT '1=customer,2=admin,3=developer',
+  `PhoneNumber` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userAddress`
+--
+
+CREATE TABLE `userAddress` (
+  `addressID` int(11) NOT NULL,
+  `AddressLine1` varchar(255) NOT NULL,
+  `AddressLine2` varchar(255) NOT NULL,
+  `Postcode` char(5) NOT NULL COMMENT 'must be 5 number long',
+  `City` varchar(20) NOT NULL,
+  `PhoneNumber` varchar(13) NOT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0=inactive,1=active',
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`userID`,`productID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Indexes for table `devChat`
+--
+ALTER TABLE `devChat`
+  ADD PRIMARY KEY (`devChatID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `orderDetails`
+--
+ALTER TABLE `orderDetails`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `AddressID` (`AddressID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `orderItems`
+--
+ALTER TABLE `orderItems`
+  ADD PRIMARY KEY (`orderID`,`productID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Indexes for table `orderStatusHistory`
+--
+ALTER TABLE `orderStatusHistory`
+  ADD PRIMARY KEY (`orderStatusID`),
+  ADD KEY `orderID` (`orderID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`paymentID`),
+  ADD KEY `orderID` (`orderID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productID`),
+  ADD KEY `categoryID` (`categoryID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD UNIQUE KEY `PhoneNumber` (`PhoneNumber`);
+
+--
+-- Indexes for table `userAddress`
+--
+ALTER TABLE `userAddress`
+  ADD PRIMARY KEY (`addressID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `devChat`
+--
+ALTER TABLE `devChat`
+  MODIFY `devChatID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orderDetails`
+--
+ALTER TABLE `orderDetails`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orderStatusHistory`
+--
+ALTER TABLE `orderStatusHistory`
+  MODIFY `orderStatusID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `paymentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userAddress`
+--
+ALTER TABLE `userAddress`
+  MODIFY `addressID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `devChat`
+--
+ALTER TABLE `devChat`
+  ADD CONSTRAINT `devchat_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Constraints for table `orderDetails`
+--
+ALTER TABLE `orderDetails`
+  ADD CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `orderdetails_ibfk_2` FOREIGN KEY (`AddressID`) REFERENCES `userAddress` (`addressID`);
+
+--
+-- Constraints for table `orderItems`
+--
+ALTER TABLE `orderItems`
+  ADD CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`),
+  ADD CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`orderID`) REFERENCES `orderDetails` (`orderID`);
+
+--
+-- Constraints for table `orderStatusHistory`
+--
+ALTER TABLE `orderStatusHistory`
+  ADD CONSTRAINT `orderstatushistory_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `orderDetails` (`orderID`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `orderDetails` (`orderID`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `category` (`categoryID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `userAddress`
+--
+ALTER TABLE `userAddress`
+  ADD CONSTRAINT `useraddress_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`userID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
