d2c3d82554b29cd7ac5c54e3cc02b81965ad8f98
Print Yongsuphamongkol <print.krittanut@gmail.com>
2024-11-02 09:27:47 +0700

    get /admin/orders

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,36 @@ const router = express.Router();
 const db = require("./db.js");
 
 // Get Order Details w/ Payment image (ALL orders - admin privileges) (maybe get only status that isn't 6=payment failed)
+router.get("/admin/orders", (req, res) => {
+    // if (!req.session.user || req.session.user.role != 2) {
+    //     return res.status(403).json({ error: "User doesn't have permission" }); // 403 = Forbidden
+    // }
+
+    // Join 4 tables - orderDetails, Payment, userAddress, (and a modified orderStatusHistory that gets the highest value of status)
+    const sql = `SELECT o.*, p.fileURL, ad.*  
+        FROM orderDetails AS o JOIN payment AS p 
+        ON o.orderID = p.orderID 
+        JOIN userAddress as ad 
+        ON o.addressID = ad.addressID 
+        JOIN (
+            SELECT orderID, status FROM orderStatusHistory 
+            WHERE (orderID, status) IN (SELECT orderID, MAX(status) 
+            FROM orderStatusHistory GROUP BY orderID) 
+        ) 
+        AS osh ON o.orderID = osh.orderID WHERE osh.status != 6`;
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error(err);
+            return res
+                .status(500)
+                .json({ error: "Database error while selecting orders" });
+        }
+
+        res.json(results);
+    });
+});
+
 // Get Order Items (reuse? from order.js?)
 // Get Order Status History (reuse? from order.js?)
 // POST orderStatusHistory
